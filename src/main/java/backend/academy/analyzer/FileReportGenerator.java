560bc67d9a8022566c07e0f17de2025865fbcf58
Semyon Burlak <syoma200489@gmail.com>
2024-11-17 21:37:38 +0300

    Changed import order

--- src/main/java/backend/academy/analyzer/FileReportGenerator.java
+++ src/main/java/backend/academy/analyzer/FileReportGenerator.java
@@ -1,11 +1,11 @@
 package backend.academy.analyzer;
 
-import lombok.extern.slf4j.Slf4j;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public abstract class FileReportGenerator {
